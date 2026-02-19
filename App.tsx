f7dcf831238f5539589e08afe7ae58bb60cf6673
jhyun79-beep <jhyun79@gmail.com>
2026-02-19 11:38:49 +0900

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { HashRouter, Routes, Route, useLocation } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Research from './pages/Research';
+import People from './pages/People';
+import Publications from './pages/Publications';
+
+// Scroll to top on route change
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
+const App: React.FC = () => {
+  return (
+    <HashRouter>
+      <ScrollToTop />
+      <div className="flex flex-col min-h-screen bg-gray-50 text-slate-900">
+        <Navbar />
+        <main className="flex-grow pt-20">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/research" element={<Research />} />
+            <Route path="/people" element={<People />} />
+            <Route path="/publications" element={<Publications />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </HashRouter>
+  );
+};
+
+export default App;
